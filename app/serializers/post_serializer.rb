02b38a06a0aa6241172763781a2fa156ba05969a
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-12-02 17:43:39 +0300

    Done

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,9 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :title, :content
+  attributes :title, :content, :short_content
   belongs_to :author
   has_many :tags
+
+  def short_content
+    "#{self.object.content[0..39]}..."
+  end
 end
